20f248407865d48f49bc195e6fc48cff7fcc942d
tsegadev <tsega.get.grace@gmail.com>
2022-07-11 03:02:22 +0100

    blog post-detail has done

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,11 @@
-from xml.dom.minidom import Identified
-from django.shortcuts import render
-from django.urls import reverse
-from datetime import date
+from django.shortcuts import render, get_object_or_404 
 from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponsePermanentRedirect
 from django.template.loader import render_to_string
 
 
 from .models import Post
+ 
 
-all_posts = [
-   
-]
-def get_date(post):
-    return post['date']
-    
 # Create your views here.
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3] 
@@ -22,18 +14,28 @@ def starting_page(request):
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html",{
         "all_posts":all_posts
     })
 
 def post_detail(request, slug):
-    try:
-        identified_posts = next(post for post in all_posts if post['slug']==slug)
-        return render(request, "blog/post-detail.html", {
-            "post" : identified_posts
-        })
-    except:
-       # raise Http404()
-       response_data =  render_to_string("404.html") 
-       return HttpResponseNotFound(response_data) 
+    
+    identified_post = get_object_or_404(Post, slug = slug)
+    return render(request, "blog/post-detail.html", {
+            "post" : identified_post,
+            "post_tags":identified_post.tags.all()
+    })   
+    
+    # try:
+    #     # identified_post = Post.objects.get(slug = slug)
+    #     # identified_posts = next(post for post in all_posts if post['slug']==slug)
+    #     identified_post = get_list_or_404(Post, slug = slug)
+    #     return render(request, "blog/post-detail.html", {
+    #         "post" : identified_post
+    #     })
+    # except:
+    #    # raise Http404()
+    #    response_data =  render_to_string("404.html") 
+    #    return HttpResponseNotFound(response_data) 
             
