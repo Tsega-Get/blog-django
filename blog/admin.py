c02a504a7e828435386bcff580bc9277d41cd7ed
*tsegadev* <*tsega.get.grace@gmail.com*>
2022-10-21 18:50:11 +0300

    Comment and read later has done

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tag, Author, Post
+from .models import Tag, Author, Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_filter = ("author","tags","date",)
     list_display = ("title","author","date",)
     prepopulated_fields= {"slug":("title",)}
-    
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name","post","text",)
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment,CommentAdmin)
--- my_site/settings.py
+++ my_site/settings.py
@@ -127,4 +127,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
     ] 
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/files/"
